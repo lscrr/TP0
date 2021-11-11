f5adcccfce2bece3baa84eb1529971e5b3cc7b16
titou <titou@192.168.114.248>
2021-11-11 19:33:22 +0100

    ??

---
+++ TP2_manip_CARRIE/src/Moussaka.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author titou
+ */
+public class Moussaka {
+    int nbCalories;
+    nbCalories = 0;
+    int [] Moussaka = new int [10];
+    
+    
+    
+}
---
+++ TP2_manip_CARRIE/src/Tartiflette.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author titou
+ */
+public class Tartiflette {
+    int nbCalories;
+    nbCalories = 0;
+    Tartiflette assiette1 = new Tartiflette(500) ;
+    Tartiflette assiette2 = new Tartiflette(600) ;
+    Tartiflette assiette3 = assiette2 ; 
+
+    }
+
