5593939f5b71d9f30cb523d852cedb5efb7e7dff
titou <titou@192.168.114.248>
2021-11-11 19:32:24 +0100

    ??

---
+++ TP2_convertisseurObjet_CARRIE/src/convertisseur_carrié/Convertisseur.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package convertisseur_carrié;
+
+/**
+ *
+ * @author carrié lisa 
+ */
+
+// je n'arrive pas à ourvir mon ex 2 du tp1
+
+public class Convertisseur {
+    int nbConversions;
+    double Convertisseur;
+    double CelciusVersKelvin;
+    double KelvinVersCelcius;
+    double FarenheitVersCelcius;
+    double CelciusVersFarenheit;
+    double FarenheitVersKelvin;
+    double KelvinVersFarenheit;
+    public Convertisseur(){
+        nbConversions=0;
+        for (int i=0; i< Convertisseur ; i++){
+        nbConversions= i +1;
+}
+    @Override
+    public String toString () {
+ return "nb de conversions"+ nbConversions;
+}
+
+    }
+    }
+}
+
