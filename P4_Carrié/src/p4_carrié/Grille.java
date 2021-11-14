6de9ab90796b3b2160df5e9ed83236adda3f21a3
titou <titou@192.168.114.248>
2021-11-14 17:49:27 +0100

    grille 	ajouterJetonDansColonne

--- P4_Carrié/src/p4_carrié/Grille.java
+++ P4_Carrié/src/p4_carrié/Grille.java
@@ -6,8 +6,25 @@ package p4_carrié;
 
 /**
  *
- * @author titou
+ * @author lisa carrié
  */
 public class Grille {
+    Cellule CellulesJeu [][]= new Cellule [6][7];
     
+    public Grille (){
+        CellulesJeu = new Cellule [6][7];
+    }
+    
+    public  boolean ajouterJetonDansColonne (Jeton j, int col){
+        if (CellulesJeu[0][col]!= null){
+        return false;
+        }else{
+            for (int i=0; i<=5; i++){
+                if (CellulesJeu[i][col]!=null){
+                    CellulesJeu[i-1][col].affecterJeton(j);
+                    return true;
+                }
+            }
+        }
+    }
 }
