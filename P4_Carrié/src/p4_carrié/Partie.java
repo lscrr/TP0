5260874f48b5d5fae31f9b800cc964a288cc99d3
titou <titou@192.168.114.248>
2021-11-19 11:10:46 +0100

    test dans main

--- P4_Carrié/src/p4_carrié/Partie.java
+++ P4_Carrié/src/p4_carrié/Partie.java
@@ -11,24 +11,38 @@ package p4_carrié;
 public class Partie {
     Joueur [] ListeJoueurs = new Joueur [2];
     Joueur joueurCourant;
-    Grille grilleJeu;
+    Grille grilleJeu = new Grille();
     
     public Partie (Joueur J1, Joueur J2){
      
     }
     
     public void attribuerCouleursAuxJoueurs (){ 
+        
         ListeJoueurs[0].affecterCouleur("Jaune");
         ListeJoueurs[1].affecterCouleur("Rouge");
     }
         
     
     public void initialiserPartie(){
-        
+        grilleJeu = new Grille();
+        attribuerCouleursAuxJoueurs();
+        Jeton j = new Jeton ("Jaune");
+        ListeJoueurs[0].ajouterJeton(j);
+        Jeton g = new Jeton ("Rouge");
+        ListeJoueurs[1].ajouterJeton(g); 
     }
     
     public void debuterPartie(){
-        
+        joueurCourant=ListeJoueurs[0];
+        while (grilleJeu.etreGagnantePourJoueur(ListeJoueurs[0])==false && grilleJeu.etreGagnantePourJoueur(ListeJoueurs[1])== false && grilleJeu.etreRemplie()==false){
+            Jeton x;
+            x = ListeJoueurs[0].ListeJetons[0];
+            grilleJeu.ajouterJetonDansColonne(x,col);
+            
+            
+                    
+       }
     }
     
 }
