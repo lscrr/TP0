1077d043559b6f8748f1d7c103cf6ceec084159b
titou <titou@192.168.114.248>
2021-11-30 19:31:16 +0100

    Test de la partie, quelques erreurs modifiées sur les différentes classes, j'essaie toujours de les corriger

--- P4_Carrié/src/p4_carrié/Grille.java
+++ P4_Carrié/src/p4_carrié/Grille.java
@@ -24,7 +24,7 @@ public class Grille {
         if (CellulesJeu[0][col].jetonCourant!= null){
             return false;
         }else{
-            for (int i=5; i>=0; i++){
+            for (int i=5; i>=0; i--){
                 if (CellulesJeu[i][col].jetonCourant==null){
                     CellulesJeu[i][col].affecterJeton(j);
                 return true;
@@ -56,20 +56,17 @@ public class Grille {
             for (int j=0; j<=6; j++){
                 System.out.print(" " +CellulesJeu[i][j].lireCouleurDuJeton()+ " " );
             }System.out.println();
-        }//couleurs trous noirs
+           
+        }
+
+//couleurs trous noirs
     }
     
     public boolean celluleOccupee (int a, int b){
-        for  (a=0; a<=5; a++){
-            for ( b=0 ; b<=6; b++){
-                if (CellulesJeu[a][b]==null){
-                    return false;
-                }else{
-                    return true;
-                }
-            }
+        if (CellulesJeu[a][b]==null){
+            return false;
         }
-        return true;
+            return true;
     }
     
     public String lireCouleurDuJeton (int  a , int b){
@@ -78,9 +75,9 @@ public class Grille {
 
     public boolean etreGagnantePourJoueur (Joueur G){
         String c=G.Couleur;
-        for(int x=0; x>=0 && x<=5; x++){
+        for(int x=0; x>= 0 && x<=5; x++){
             for (int y=0; y>=0 && y<=3; y++){ // verifier si une ligne est gagnante
-                if (c==CellulesJeu[x][y+1].lireCouleurDuJeton() 
+                if ( c==CellulesJeu[x][y+1].lireCouleurDuJeton() 
                         && c==CellulesJeu[x][y+2].lireCouleurDuJeton() 
                         && c==CellulesJeu[x][y+3].lireCouleurDuJeton()){
                 return true;
@@ -88,27 +85,27 @@ public class Grille {
     }
         }
         for (int x=0; x>=0 && x<=2; x++){ //verifier si une colonne est gagnante
-            for (int y=0;y>=0 && y<=6; y++){
+            for (int y=0; y>= 0 && y<=6; y++){
                 if (c==CellulesJeu[x+1][y].lireCouleurDuJeton() 
                         && c==CellulesJeu[x+2][y].lireCouleurDuJeton() 
-                        && c==CellulesJeu[x+3][y].lireCouleurDuJeton()){
+                        &&c==CellulesJeu[x+3][y].lireCouleurDuJeton()){
                 return true;
                     }
             }
         }
-        for(int x=0;  x>=0 && x<=2; x++){
-            for (int y=0; y>=0 && y<=3; y++){ // verifier si une diagonale montante est gagnante
+        for(int x=0; x>=0 && x<=2; x++){
+            for (int y=0; y>= 0 && y<=3; y++){ // verifier si une diagonale montante est gagnante
                 if ( c==CellulesJeu[x+1][y+1].lireCouleurDuJeton() 
                         && c==CellulesJeu[x+2][y+2].lireCouleurDuJeton() 
-                        && c==CellulesJeu[x+3][y+3].lireCouleurDuJeton()){
+                        &&c==CellulesJeu[x+3][y+3].lireCouleurDuJeton()){
                 return true;
             }
             }
             }
-        for(int x=0;x>=3 && x<=5; x++){
-            for (int y=0; y>=0 && y<=3; y++){ // verifier si une diagonale descendante est gagnante
-                if (c==CellulesJeu[x-1][y+1].lireCouleurDuJeton() 
-                        && c==CellulesJeu[x-2][y+2].lireCouleurDuJeton() 
+        for(int x=0;x>=1 && x<=5; x++){
+            for (int y=0;y<=3; y++){ // verifier si une diagonale descendante est gagnante
+                if (c==CellulesJeu[x-1][y+1].lireCouleurDuJeton()
+                        && c==CellulesJeu[x-2][y+2].lireCouleurDuJeton()
                         && c==CellulesJeu[x-3][y+3].lireCouleurDuJeton()){
                     
                 return true;
