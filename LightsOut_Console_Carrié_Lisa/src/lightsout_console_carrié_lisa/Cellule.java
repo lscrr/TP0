578941de9ef37a99ca647636277b9a1a9caba62f
titou <titou@192.168.114.248>
2021-12-16 13:34:42 +0100

    création des méthodes de la classe cellule

--- LightsOut_Console_Carrié_Lisa/src/lightsout_console_carrié_lisa/Cellule.java
+++ LightsOut_Console_Carrié_Lisa/src/lightsout_console_carrié_lisa/Cellule.java
@@ -10,4 +10,42 @@ package lightsout_console_carrié_lisa;
  */
 public class Cellule {
     
-}
+    boolean état;
+    boolean c;
+    
+    public boolean étatCellule (String cellule){
+        if (état= true){
+            cellule = "allumée";
+        }else{
+            cellule = "éteinte";
+        }
+        return false;
+    }
+    
+    public boolean allumerCellule(boolean celluleAllumée){
+            if ( état == false){
+                état = étatcellule;
+                return false;
+            }
+            return true;
+        }
+    
+    public void éteindreCellule(boolean celluleEteinte){
+        if (état==true){
+            état = celluleEteinte;
+        }
+    }
+    
+    public boolean changerEtat(boolean cellule){
+        if (cellule.allumerCellule() = true){
+            cellule.éteindreCellule();
+        }else{
+            cellule.allumerCellule();
+        }
+    }
+    }
+    
+    
+    
+    
+
