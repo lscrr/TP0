277a2fedc32d475c686da5f2130c7cd7ebbb6e23
titou <titou@192.168.114.248>
2021-12-17 10:52:33 +0100

    création des méthodes de la classe grille

--- LightsOut_Console_Carrié_Lisa/src/lightsout_console_carrié_lisa/Grille.java
+++ LightsOut_Console_Carrié_Lisa/src/lightsout_console_carrié_lisa/Grille.java
@@ -9,16 +9,82 @@ package lightsout_console_carrié_lisa;
  * @author lisa carrié
  */
 public class Grille {
-    Cellule [][] Lumière = new Cellule [5][5];
-   // boolean état;
+    Cellule [][] CellulesLumineuses = new Cellule [5][5];
+    Cellule CelluleCourante;
     
     public void afficherGrilleSurConsole(){
         for (int i =0; i<5 ; i++){
             for (int j=0; j<5; j++){
-                System.out.print(" " +Lumière [i][j].lumière + " ");
+                System.out.print(" " +CellulesLumineuses [i][j].état + " ");
             }System.out.println();
         }
     }
+    public boolean grilleGagnante( Cellule CelluleCourante){
+        for (int i=0; i<5; i++){
+            for (int j=0; j<5; j++){
+                if (CellulesLumineuses[i][j].étatCellule()== true){
+                return false;   
+            }
+        }
+        }
+        return true;
+    }
     
-    public 
-}
+    public void allumerGrille(){
+        
+    }
+    
+    public void éteindreGrille(){
+        
+    }
+    
+    public void présenceDeLumière(int i, int j){
+        
+        if (i>0 && i<4 && j>0 && j<4){                   // centre de la grille: on change l'état de la cellule une fois les coordonées du joueur sélectionnées
+            CellulesLumineuses[i][j+1].changerEtat();
+            CellulesLumineuses[i][j-1].changerEtat();
+            CellulesLumineuses[i+1][j].changerEtat();
+            CellulesLumineuses[i-1][j].changerEtat();
+                                                             // on s'attaque à présent aux coins de la grile
+        } else if ( i==0 && j==0 ){                    // pour le cas du point (0,0) 
+            CellulesLumineuses[i][j+1].changerEtat();
+            CellulesLumineuses [i+1][j].changerEtat();
+            
+        } else if ( i == 0 && j==4){                   // pour le cas du point (0,4)
+            CellulesLumineuses[i+1][j].changerEtat();
+            CellulesLumineuses[i][j-1].changerEtat();
+            
+        }else if (i==4 && j==0){                         // pour le cas du point (4,0)
+            CellulesLumineuses[i][j+1].changerEtat();
+            CellulesLumineuses[i-1][j].changerEtat();
+            
+        }else if (i==4 && j==4){                          // pour le cas du point (4,4)
+            CellulesLumineuses[i][j-1].changerEtat();
+            CellulesLumineuses[i-1][j].changerEtat();
+                                                        // à présent, on isole les cellules sur les bords de la grille (en dehors des coins)
+        } else if ((i>0 && i<4) && j==0){             // pour les cellules du haut de la grille
+            CellulesLumineuses[i-1][j].changerEtat();
+            CellulesLumineuses [i+1][j].changerEtat();
+            CellulesLumineuses[i][j+1].changerEtat();
+            
+        }else if ( i==0 && (j>0 && j<4)){       // pour les cellules du coté gauche de la grille
+            CellulesLumineuses[i][j+1].changerEtat();
+            CellulesLumineuses[i][j-1].changerEtat();
+            CellulesLumineuses[i+1][j].changerEtat();
+            
+        } else if (i ==4 && (j>0 && j<4)){           // pour les cellules du coté droit de la grille
+            CellulesLumineuses[i][j-1].changerEtat();
+            CellulesLumineuses[i][j+1].changerEtat();
+            CellulesLumineuses[i-1][j].changerEtat();
+        } else if ((i>0 && i<4) && j==4){                       // pour les cellules du bas de la grille
+                      CellulesLumineuses[i][j-1].changerEtat();
+                      CellulesLumineuses[i+1][j].changerEtat();
+                      CellulesLumineuses[i-1][j].changerEtat();
+        }
+    
+    
+        }
+    
+ }
+    
+
