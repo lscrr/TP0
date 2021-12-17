35e0a8afae620d296bbfe2150866ef889f13dde2
titou <titou@192.168.114.248>
2021-12-17 11:15:03 +0100

    création des méthodes de la classe partie ( à terminer)

--- LightsOut_Console_Carrié_Lisa/src/lightsout_console_carrié_lisa/Partie.java
+++ LightsOut_Console_Carrié_Lisa/src/lightsout_console_carrié_lisa/Partie.java
@@ -4,10 +4,24 @@
  */
 package lightsout_console_carrié_lisa;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author lisa carrié
  */
 public class Partie {
-    
+   Grille grilleLumière =new Grille();
+   Random lumière = new Random();
+   Scanner sc= new Scanner(System.in);
+   
+   public void initialiserPartie(){
+      while (grilleLumière.grilleGagnante()!=true){
+          System.out.println("Selectionnez les coordonnées de la cellule dans laquelle vous souhaitez jouer"); 
+      }
+   }
+   grg= rand.nextInt(26);
+   
 }
+c 
